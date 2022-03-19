4065b148bcbbf814d56e5d1011aac535ec79f0fa
Anton Semenikhin <protest_the_hero@mail.ru>
2022-03-19 13:00:48 +0300

    hide scrollbar

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -7,64 +7,61 @@ export const modal = () => {
 	const modal = document.querySelector('.header-modal')
 	const overlay = document.querySelector('.overlay')
 
-	const modalRight = () => {
-		animate({
-			duration: 150,
-			timing(timeFraction) {
-				return timeFraction;
-			},
-			draw(progress) {
-				overlay.style.display = "block"
-				overlay.style.opacity = progress
-				modal.style.display = "block"
-				modal.style.top = 50 + "%"
-				modal.style.left = (50 * progress) + "%";
-			}
-		});
-	};
 
-	const modalDown = () => {
-
-		animate({
-			duration: 200,
-			timing(timeFraction) {
-				return timeFraction;
-			},
-			draw(progress) {
-				progress == 1 ? overlay.style.display = "none" :
-					overlay.style.opacity = 1 - progress;
-				modal.style.top = (50 + progress * 100) + "%";
-			}
-		});
-	};
-
-	const modalMobileOpen = () => {
-		overlay.style.display = "block"
-		overlay.style.opacity = "1"
-		modal.style.top = 50 + "%"
-		modal.style.display = "block"
+	const modalOpen = () => {
+		document.body.classList.toggle('scroll-hide')
+		console.log(document.body.clientWidth)
+		console.log(innerWidth)
+		if (innerWidth < 768) {
+			overlay.style.display = "block"
+			overlay.style.opacity = "1"
+			modal.style.top = 50 + "%"
+			modal.style.display = "block"
+		} else {
+			animate({
+				duration: 150,
+				timing(timeFraction) {
+					return timeFraction;
+				},
+				draw(progress) {
+					overlay.style.display = "block"
+					overlay.style.opacity = progress
+					modal.style.display = "block"
+					modal.style.top = 50 + "%"
+					modal.style.left = (50 * progress) + "%";
+				}
+			})
+		}
 	}
 
-	const modalMobileClose = () => {
-		overlay.style.display = "none"
-		overlay.style.opacity = "1"
-		modal.style.display = "none"
-		modal.style.top = 50 + "%"
+	const modalClose = () => {
+		document.body.classList.toggle('scroll-hide')
+		if (innerWidth < 768) {
+			overlay.style.display = "none"
+			overlay.style.opacity = "1"
+			modal.style.display = "none"
+			modal.style.top = 50 + "%"
+		} else {
+			animate({
+				duration: 200,
+				timing(timeFraction) {
+					return timeFraction;
+				},
+				draw(progress) {
+					progress == 1 ? overlay.style.display = "none" :
+						overlay.style.opacity = 1 - progress;
+					modal.style.top = (50 + progress * 100) + "%";
+				}
+			});
+		}
+
 	}
 	document.body.addEventListener('click', (e) => {
-		// e.preventDefault()
 		if (e.target.closest('.button > [href="#callback"]')) {
-			if (innerWidth < 768) {
-				modalMobileOpen()
-			} else {
-				modalRight()
-			}
+			e.preventDefault()
+			modalOpen()
 		} else if (e.target.classList.contains('header-modal__close') || e.target === overlay) {
-			if (innerWidth < 768) {
-				modalMobileClose()
-			} else {
-				modalDown()
-			}
+			modalClose()
 		}
 	})
 }
